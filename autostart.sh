2bc39be7376017c97bc3534bb85f04ef3019e589
m4jbz <zeroknel@proton.me>
2023-07-31 13:28:16 -0600

    backup of dwm

---
+++ autostart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
+  then
+    $@&
+  fi
+}
+
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+##Wallaper
+feh --bg-fill ~/Pictures/Wallpapers/003.png
+
+#Los key bindings
+run sxhkd -c ~/.local/suckless/dwm/sxhkdrc &
+
+#starting utility applications at boot time
+run xfce4-power-manager &
+run nm-applet &
+run volumeicon &
+run cbatticon &
+/bin/sh ~/xroot.sh &
+picom --config $HOME/Documents/Picom/picom.conf &
